1546eefcea3d228e0e92a9d84c009219c1583e78
AndreyChebulaev <andrejceb044@gmail.com>
2024-10-31 22:19:07 +0500

    Добавьте файлы проекта.

---
+++ http/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class SecurityUpdatesFetcher
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+
+    // Метод для получения обновлений по HTTP
+    public async Task<string> GetUpdateViaHttpAsync(string url)
+    {
+        try
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"HTTP Error: {ex.Message}");
+            return null;
+        }
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        SecurityUpdatesFetcher fetcher = new SecurityUpdatesFetcher();
+
+        // Получение обновлений через HTTP
+        string httpUrl = "http://alizar.habrahabr.ru/";
+        string httpUpdate = await fetcher.GetUpdateViaHttpAsync(httpUrl);
+        Console.WriteLine("HTTP Update:\n" + httpUpdate);
+    }
+}
