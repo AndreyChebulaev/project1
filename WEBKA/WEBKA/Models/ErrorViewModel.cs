9140cdeef11610505b365536fbc05b7af114d24d
AndreyChebulaev <andrejceb044@gmail.com>
2025-03-13 20:04:42 +0500

    upd2

--- WEBKA/WEBKA/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WEBKA.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
